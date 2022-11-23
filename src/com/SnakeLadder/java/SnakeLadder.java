17a5209c682307d6c4fcdd5f261dfc12e932ef1e
kapilmahajan123 <montyempire123gmail.com>
2022-11-23 11:52:58 +0530

    Added: Starting Position

---
+++ src/com/SnakeLadder/java/SnakeLadder.java
@@ -0,0 +1,24 @@
+package com.SnakeLadder.java;
+
+/**
+ * 
+ * @author Kapil
+ *
+ */
+public class SnakeLadder {
+	
+	public static void main(String[] args) {
+		int start;
+		
+		/*
+		 * player starting point
+		 */
+		start =  0; 
+		
+		//print the starting position 
+		System.out.println("Player Starting Position will be:~ ");
+		System.out.println(+start);   //just print out to start value
+		
+	}
+
+}
